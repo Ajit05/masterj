e10bd328be6f3a674ece023baf694ed3a81da4b8
Ajit05 <ajity050@gmail.com>
2017-02-13 22:23:53 +0530

    Daily Task System

---
+++ TaskStatus/src/TaskDaily/TaskD.java
@@ -0,0 +1,20 @@
+package TaskDaily;
+
+import javax.swing.JFrame;
+
+public class TaskD extends JFrame  {
+	
+	public TaskD(){
+		// TODO Auto-generated method stub
+		System.out.println("Ajit");
+		setSize(1000,500);
+		setTitle("Daily Task");
+		setResizable(true);
+		setVisible(true);
+	}
+	public static void main(String args[]){
+		TaskD obj = new TaskD();
+		
+	}
+	
+}
